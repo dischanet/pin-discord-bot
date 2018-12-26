070f3150fc79f754dd928e630b2d3b8f42be95a2
Yusuke Hayashi <yusuke8h@gmail.com>
2018-12-26 21:53:42 +0900

    環境変数名もPINに変更

--- hooks/install.sh
+++ hooks/install.sh
@@ -4,7 +4,7 @@ cd /home/ec2-user/repos/pin-discord-bot
 
 ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
 REGION=$(echo ${ZONE/%?/})
-PARAMETER_NAME=JS_DISCORD_BOT_SECRET
+PARAMETER_NAME=PIN_DISCORD_BOT_SECRET
 echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
 
 sudo cp ./hooks/pin-discord-bot.service /etc/systemd/system/pin-discord-bot.service
