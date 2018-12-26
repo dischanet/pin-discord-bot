de7724b0373169c64e3601fff3bc0e9ae7e652ff
Yusuke Hayashi <yusuke8h@gmail.com>
2018-12-26 17:09:01 +0900

    serviceをcpする

--- hooks/install.sh
+++ hooks/install.sh
@@ -7,5 +7,5 @@ REGION=$(echo ${ZONE/%?/})
 PARAMETER_NAME=JS_DISCORD_BOT_SECRET
 echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
 
-sudo ln -s ./hooks/js-discord-bot.service /etc/systemd/system/js-discord-bot.service
+sudo cp ./hooks/js-discord-bot.service /etc/systemd/system/js-discord-bot.service
 sudo /usr/bin/systemctl enable js-discord-bot
