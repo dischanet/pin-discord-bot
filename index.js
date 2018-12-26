914b2070dec68986cbceb23788f310d9bfca8688
hal-shu-sato <hal_shu_sato@yahoo.co.jp>
2018-12-26 22:17:13 +0900

    [追加] pininfoコマンドでBOTの概要を表示するように変更

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
+  if (msg.content === "pininfo") {
+    msg.reply("ピン留めくん - ピン留めBOT made by Dischanet Team");
+  }
 });
 
 client.login(process.env['DISCORD_BOT_TOKEN']);
