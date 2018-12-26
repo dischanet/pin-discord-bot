b38725a94fba12e745c765807456190ecc93d508
ato lash <hal-shu-sato@users.noreply.github.com>
2018-12-26 23:36:59 +0900

    [追加] ピンのリアクションでピン留めする機能を追加 (#4)

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
+  } else if (msg.content === 'pininfo') {
+    msg.reply('ピン留めくん - ピン留めBOT made by Dischanet Team');
+  }
+});
+
+client.on('messageReactionAdd', reacta => {
+  if (reacta.emoji.name === 'pushpin') {
+    reacta.message.pin();
+  }
+});
+
+client.on('messageReactionRemove', reactr => {
+  if (reactr.emoji.name === 'pushpin') {
+    reactr.message.unpin();
   }
   if (msg.content === 'pininfo') {
     msg.reply('ピン留めくん - ピン留めBOT made by Dischanet Team');
