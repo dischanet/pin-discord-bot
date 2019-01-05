1a49306d9f1012d11d4d837ea148ee7094439184
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-05 21:16:20 +0900

    [修正]デプロイサーバーの変更とeslintの追加

--- hooks/install.sh
+++ hooks/install.sh
@@ -2,10 +2,9 @@
 
 cd /home/ec2-user/repos/pin-discord-bot
 
-ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
-REGION=$(echo ${ZONE/%?/})
+REGION=$(curl -s 169.254.169.254/latest/meta-data/local-hostname | cut -d '.' -f2)
 PARAMETER_NAME=PIN_DISCORD_BOT_SECRET
-echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
+echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameter --name ${PARAMETER_NAME} --query "Parameter.Value" --output text)" > environment
 
-sudo cp ./hooks/pin-discord-bot.service /etc/systemd/system/pin-discord-bot.service
-sudo /usr/bin/systemctl enable pin-discord-bot
+cp ./hooks/pin-discord-bot.service /etc/systemd/system/pin-discord-bot.service
+/usr/bin/systemctl enable pin-discord-bot
